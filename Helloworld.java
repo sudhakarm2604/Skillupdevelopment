ddb34566e58dd10ff6f0c30b6048e790352b5852
sudhakarm2604 <125337683+sudhakarm2604@users.noreply.github.com>
2023-02-14 15:52:17 +0530

    Changed the print text

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ public class JenkinsExample {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hi Sudhakar");
- System.out.println("Be positive , your brain is more than powerfull you think");
+ System.out.println("Be positive , your mind is more than powerfull you think");
 
  }
 
