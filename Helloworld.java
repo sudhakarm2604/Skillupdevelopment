48cff91836091e4ad511bb081961eb2c679ce0c8
sudhakarm2604 <125337683+sudhakarm2604@users.noreply.github.com>
2023-02-14 15:51:11 +0530

    Initial commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class JenkinsExample {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hi Sudhakar");
+ System.out.println("Be positive , your brain is more than powerfull you think");
+
+ }
+
+}
