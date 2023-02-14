381789be74e80bd6be6185675c7381fc0aa25133
Sudhakarms2604 <125361853+Sudhakarms2604@users.noreply.github.com>
2023-02-14 19:33:37 +0530

    Added survey feature

--- Helloworld.java
+++ Helloworld.java
@@ -7,6 +7,7 @@ public class JenkinsExample {
   System.out.println("Adding the live class feature");
  System.out.println("Adding the online live class");
   System.out.println("Adding the exam feature along with mocktest3");
+  System.out.println("Adding survey");
  }
 
 }
